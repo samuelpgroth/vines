c93b72034319db93ac465df63344f978882ff763
Samuel Groth <samuelpgroth@gmail.com>
2020-07-01 09:55:55 +0100

    Add gitignore and modify setup

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
+def readme():
+    with open("README.md", "r") as f:
+        return f.read()
+
 
 setuptools.setup(
-    name="vines", # Replace with your own username
+    name="vines",
     version="0.0.1",
     author="Samuel Groth",
     author_email="samuelpgroth@gmail.com",
     description="Volume INtegral Equation Solver (VINES)",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
     url="https://github.com/samuelpgroth/vines",
     packages=setuptools.find_packages(),
     classifiers=[
@@ -16,5 +21,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    package_dir={'':'vines'},
     python_requires='>=3.6',
+    install_requires=['pyfftw','numba'],
+    include_package_data=True
 )
