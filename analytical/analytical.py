1e6f1a6fa50133a4fac0e761be3d9646aa869d3f
Samuel Groth <samuelpgroth@gmail.com>
2021-01-28 11:57:16 +0000

    Add three notebook demos to acoustics folder.

--- analytical/analytical.py
+++ analytical/analytical.py
@@ -2,7 +2,7 @@
 # i.e., with the Neumann data set to zero on the circle boundary.
 # Samuel Groth
 # Cambridge, 20/11/19
-
+from numba import njit, prange
 
 def sound_hard_circle(k, rad, plot_grid):
     # from pylab import find
@@ -123,6 +123,75 @@ def penetrable_circle(k0, k1, rad, plot_grid):
 
         u_ext += a_n * hankel1(n, k0 * r) * np.exp(1j * n * theta)
         u_int += b_n * jv(n, k1 * r) * np.exp(1j * n * theta)
+
+    u_int[n_ext] = 0.0
+    u_ext[n_int] = 0.0
+    u_sc = u_int + u_ext
+    u_scat = u_sc.reshape(Nx, Ny)
+    u_tot = u_scat + u_plot
+
+    return u_tot
+
+# FIXME: attempt at numba implementation. Not working currently.
+def penetrable_circle_numba(k0, k1, rad, plot_grid):
+    # from pylab import find
+    from scipy.special import jv, hankel1
+    import numpy as np
+    x = np.vstack((plot_grid[0].ravel(), plot_grid[1].ravel()))
+    points = x
+    fem_xx = points[0, :]
+    fem_xy = points[1, :]
+    r = np.sqrt(fem_xx * fem_xx + fem_xy * fem_xy)
+    theta = np.arctan2(fem_xy, fem_xx)
+    npts = np.size(fem_xx, 0)
+    a = rad
+
+    n_terms = np.max([200, np.int(55 + (k0 * a)**1.01)])
+
+    Nx = plot_grid.shape[1]
+    Ny = plot_grid.shape[2]
+
+    u_inc = np.exp(1j * k0 * fem_xx)
+    n_int = np.where(r < a)
+    n_ext = np.where(r >= a)
+    u_inc[n_int] = 0.0
+    u_plot = u_inc.reshape(Nx, Ny)
+
+    # u_int = np.zeros(npts, dtype=np.complex128)
+    # u_ext = np.zeros(npts, dtype=np.complex128)
+
+    @njit(parallel=True)
+    def summation(k0):
+        u_int_arr = np.zeros((npts, 2*n_terms+1), dtype=np.complex128)
+        u_ext_arr = np.zeros((npts, 2*n_terms+1), dtype=np.complex128)
+        for n in prange(-n_terms, n_terms):
+            bessel_k0 = jv(n, k0 * rad)
+            bessel_k1 = jv(n, k1 * rad)
+
+            hankel_k0 = hankel1(n, k0 * rad)
+
+            bessel_deriv_k0 = jv(n-1, k0 * rad) - n/(k0 * rad) * jv(n, k0 * rad)
+            bessel_deriv_k1 = jv(n-1, k1 * rad) - n/(k1 * rad) * jv(n, k1 * rad)
+
+            hankel_deriv_k0 = n/(k0 * rad) * hankel_k0 - hankel1(n+1, k0 * rad)
+
+            a_n = (1j**n) * (k1 * bessel_deriv_k1 * bessel_k0 -
+                            k0 * bessel_k1 * bessel_deriv_k0) / \
+                            (k0 * hankel_deriv_k0 * bessel_k1 -
+                            k1 * bessel_deriv_k1 * hankel_k0)
+            b_n = (a_n * hankel_k0 + (1j**n) * bessel_k0) / bessel_k1
+
+            u_ext_arr[:, n] = a_n * hankel1(n, k0 * r) * np.exp(1j * n * theta)
+            u_int_arr[:, n] = b_n * jv(n, k1 * r) * np.exp(1j * n * theta)
+
+            # u_ext += a_n * hankel1(n, k0 * r) * np.exp(1j * n * theta)
+            # u_int += b_n * jv(n, k1 * r) * np.exp(1j * n * theta)
+
+        u_ext = np.sum(u_ext_arr, axis=1)
+        u_int = np.sum(u_int_arr, axis=1)
+        return u_ext, u_int
+
+    u_ext, u_int = summation(k0)
 
     u_int[n_ext] = 0.0
     u_ext[n_int] = 0.0
--- demo/acoustics/old/circle.py
+++ demo/acoustics/old/circle.py
@@ -295,57 +295,7 @@ E_tot = EINC.reshape(M, N, order='F') \
 
 E = mvp_eval.reshape(M, N, order='F')
 
-# # Solve dense system
-# A_dense = np.identity(M*N) - \
-#         ko**2 * np.matmul(np.diag(MR.reshape(M*N, 1, order='F')[:, 0]), MAT)
 
-# it_count = 0
-
-# sol1, info1 = gmres(A_dense, eInc, tol=1e-4, callback=iteration_counter)
-# print("The linear system was solved in {0} iterations".format(it_count))
-
-# circ_dense = np.zeros((M*N, M*N), dtype=np.complex128)
-# for i in range(0, N-1):
-#     i_temp = i*M
-#     for j in range(0, N-1):
-#         j_temp = j*M
-#         temp_vec = MAT[i_temp, j_temp:j_temp+M]
-#         c_temp = np.zeros(M, dtype='complex128')
-#         c_temp[0] = temp_vec[0]
-#         for ii in range(1, M):
-#             c_temp[ii] = (M - ii) / M * temp_vec[ii] \
-#                     + ii/M * temp_vec[(M - 1) - ii + 1]
-#         circ_dense[i_temp:(i+1)*M, j_temp:(j+1)*M] = \
-#             toeplitz(c_temp, c_temp)
-
-# P_dense = np.identity(M*N) - \
-#         ko**2 * np.matmul(np.diag(MR.reshape(M*N, 1, order='F')[:, 0]), circ_dense)
-
-
-# mvp_eval = mvp_domain(solp, opCirc, M, N, MR)
-
-# EINC = np.zeros((M * N, 1), dtype=np.complex128)
-# EINC = np.exp(1j * ko * (np.real(x)*dInc[0] + np.imag(x*dInc[1])))
-
-# E_tot = solp.reshape(M, N, order='F')
-
-# E_tot = EINC.reshape(M, N, order='F') \
-#     - mvp_eval.reshape(M, N, order='F') \
-#     + solp.reshape(M, N, order='F')
-
-# error_l2 = np.linalg.norm(u_exact - E_tot) / np.linalg.norm(u_exact)
-# print('error = ', error_l2)
-
-
-# it_count = 0
-# start = time.time()
-# solp, info = gmres(A, eInc, M=P_dense, tol=1e-4, callback=iteration_counter)
-# print("The linear system was solved in {0} iterations".format(it_count))
-# end = time.time()
-# print('Solve time = ', end-start,'s')
-
-# print('Relative residual = ',
-#       np.linalg.norm(mvp(solp)-eInc)/np.linalg.norm(eInc))
 
 u_exact = penetrable_circle(ko, ko*refInd, rad, plot_grid)
 
